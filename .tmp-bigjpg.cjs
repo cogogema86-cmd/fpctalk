8868dfaacaf7df198fddc9f6aa04385b28d044fd
TaeGyu Kim <cogogema86@gmail.com>
2026-06-13 14:44:44 +0900

    chore: 테스트용 임시 파일 제거

--- .tmp-bigjpg.cjs
+++
@@ -1,7 +0,0 @@
-const sharp = require("sharp");
-const w=1500,h=2000, ch=3;
-const buf = Buffer.alloc(w*h*ch);
-for (let i=0;i<buf.length;i++) buf[i]=Math.floor(Math.random()*256);
-sharp(buf,{raw:{width:w,height:h,channels:ch}}).jpeg({quality:80}).toFile(".tmp-big-form.jpg")
-  .then(()=>{const fs=require("fs");console.log("size:", (fs.statSync(".tmp-big-form.jpg").size/1024/1024).toFixed(2),"MB");})
-  .catch(e=>{console.error(e);process.exit(1)});
